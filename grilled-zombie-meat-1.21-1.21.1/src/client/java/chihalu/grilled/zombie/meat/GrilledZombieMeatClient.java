570172fd1af1b34b5c07729b614a68b4552a71cd
Chihalu <m.chihiro0314@gmail.com>
2025-11-01 21:44:23 +0900

    update gradle.properties, ModItems.java and fabric.mod.json

---
+++ grilled-zombie-meat-1.21-1.21.1/src/client/java/chihalu/grilled/zombie/meat/GrilledZombieMeatClient.java
@@ -0,0 +1,10 @@
+package chihalu.grilled.zombie.meat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GrilledZombieMeatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
